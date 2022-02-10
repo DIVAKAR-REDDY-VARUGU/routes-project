aca987d18834a43b369a16a1f0f9f9b496607ff3
VARUGU DIVAKAR REDDY <divakarreddyvarugu@gmail.com>
2022-02-10 16:21:47 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import {Route, Routes,Link} from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import ContactUs from './ContactUs';
+import Profile from './Profile';
+import { useState } from 'react';
 
 function App() {
+  // const [count,setcount]=useState(0);
+  const [active,setactive]=useState('Home')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <p> no of clicks {count}</p> */}
+      {/* <button onClick={()=>setcount(count+1)}>click me </button> */}
+      <div className='Navigation'>
+        <Link to='/' onClick={()=>setactive('Home')} className={ (active=='Home')?'active':'unactive' }>Home </Link>
+        <Link to='/ContactUs'onClick={()=>setactive('contact')} className={ (active=='contact')?'active':'unactive' }>contact us </Link>
+        <Link to='/Profile'onClick={()=>setactive('profile')} className={ (active=='profile')?'active':'unactive' }>Profile </Link>
+      </div>
+      <Routes>
+        {/* <Route path='/' element={<>hello bro notthing is there here </>} /> */}
+        <Route path='/' element={<Home/>} />
+        <Route path='/ContactUs' element={<ContactUs/>} />
+        <Route path='/Profile' element={<Profile/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Profile.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Profile() {
+  return <div>
+      < p> this is profile page ...</p>
+  </div>;
+}
